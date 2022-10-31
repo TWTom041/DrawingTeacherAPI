8b9e15115b392793004707cb9909e584f0bc8a2f
TWTom <tom0986820022@gmail.com>
2022-10-31 21:04:39 +0800

    1

--- outline_get.py
+++ outline_get.py
@@ -1,105 +1,64 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('stylized.jpg')
-
-
-class line:
-    points = []
-
-    def append(self, point):
-        self.points.append(point)
-
 
 def get_outline(image: np.ndarray, method=None):
     method = method if method is not None else "canny_blurred"
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     if method == "canny":
         return cv2.Canny(gray, 30, 150)
-
     blurred = cv2.GaussianBlur(gray, (5, 5), 0)
     canny_blurred = cv2.Canny(blurred, 30, 150)
     return canny_blurred
 
 
-def group(image: np.ndarray):
-    points = np.argwhere(image != 0).tolist()
-    line_group = []  # [{points: [], extreme: []}, ...]  extreme usually is
-    while len(points) != 0:
-        flag = True
-        line_group.append({"points": [points[0]], "extreme": []})
-        # get closest main loop
-        while True:
-            front_flag = False
-            # left up is on left side
-            # **.
-            # *@.
-            # *..
-            for y in range(-1, 2):
-                if [line_group[-1]["points"][0][0] + y, line_group[-1]["points"][0][1] - 1] in points:
-                    front_flag = True
-                    line_group[-1]["points"].insert(0, (line_group[-1]["points"][0][0] + y,
-                                                        line_group[-1]["points"][0][1] - 1))
-                    points.remove([line_group[-1]["points"][0][0] + y, line_group[-1]["points"][0][1] - 1])
-            if [line_group[-1]["points"][0][0] - 1, line_group[-1]["points"][0][1]] in points:
-                front_flag = True
-                line_group[-1]["points"].insert(0, (line_group[-1]["points"][0][0] - 1,
-                                                    line_group[-1]["points"][0][1]))
-                points.remove([line_group[-1]["points"][0][0] - 1, line_group[-1]["points"][0][1]])
-            # right bottom is on left side
-            # ..*
-            # .@*
-            # .**
-            for y in range(-1, 2):
-                if [line_group[-1]["points"][0][0] + y, line_group[-1]["points"][0][1] + 1] in points:
-                    front_flag = True
-                    line_group[-1]["points"].insert(0, (line_group[-1]["points"][0][0] + y,
-                                                        line_group[-1]["points"][0][1] + 1))
-                    points.remove([line_group[-1]["points"][0][0] + y, line_group[-1]["points"][0][1] + 1])
-            if [line_group[-1]["points"][0][0] + 1, line_group[-1]["points"][0][1]] in points:
-                front_flag = True
-                line_group[-1]["points"].insert(0, (line_group[-1]["points"][0][0] + 1,
-                                                    line_group[-1]["points"][0][1]))
-                points.remove([line_group[-1]["points"][0][0] + 1, line_group[-1]["points"][0][1]])
-            end_flag = False
-            # left up is on left side
-            # **.
-            # *@.
-            # *..
-            for y in range(-1, 2):
-                if [line_group[-1]["points"][-1][0] + y, line_group[-1]["points"][-1][1] - 1] in points:
-                    end_flag = True
-                    line_group[-1]["points"].insert(len(line_group[-1]["points"]) - 1,
-                                                    (line_group[-1]["points"][0][0] + y,
-                                                     line_group[-1]["points"][0][1] - 1))
-                    points.remove([line_group[-1]["points"][0][0] + y, line_group[-1]["points"][0][1] - 1])
-            if [line_group[-1]["points"][0][0] - 1, line_group[-1]["points"][0][1]] in points:
-                end_flag = True
-                line_group[-1]["points"].insert(len(line_group[-1]["points"]) - 1,
-                                                (line_group[-1]["points"][0][0] - 1,
-                                                 line_group[-1]["points"][0][1]))
-                points.remove([line_group[-1]["points"][0][0] - 1, line_group[-1]["points"][0][1]])
-            # right bottom is on left side
-            # ..*
-            # .@*
-            # .**
-            for y in range(-1, 2):
-                if [line_group[-1]["points"][0][0] + y, line_group[-1]["points"][0][1] + 1] in points:
-                    end_flag = True
-                    line_group[-1]["points"].insert(len(line_group[-1]["points"]),
-                                                    (line_group[-1]["points"][0][0] + y,
-                                                     line_group[-1]["points"][0][1] + 1))
-                    points.remove([line_group[-1]["points"][0][0] + y, line_group[-1]["points"][0][1] + 1])
-            if [line_group[-1]["points"][0][0] + 1, line_group[-1]["points"][0][1]] in points:
-                end_flag = True
-                line_group[-1]["points"].insert(len(line_group[-1]["points"]),
-                                                (line_group[-1]["points"][0][0] + 1,
-                                                 line_group[-1]["points"][0][1]))
-                points.remove([line_group[-1]["points"][0][0] + 1, line_group[-1]["points"][0][1]])
+def group(image):
+    def calculate_geocenter(dot_indexes):
+        return tuple(s/len(dot_indexes) for s in list(map(sum, list(map(list, zip(*dot_indexes))))))
+
+    def get_neighbor(coor, maps):
+        checker = [(-1, 0), (1, 0), (-1, -1), (0, -1), (1, -1), (-1, 1), (0, 1), (1, 1)]
+        oute = []
+        for i in checker:
+            target = (i[0] + coor[0], i[1] + coor[1])
+            if target in maps:
+                oute.append(target)
+                maps.remove(target)
+        return oute, maps
+
+    def get_map(npimg):
+        outee = []
+        for i, p in np.ndenumerate(npimg):
+            if p != 0:
+                outee.append(i)
+        return outee
+
+    groups = []
+    maps_all = get_map(image)
+    while maps_all:
+        gp = {"geocenter": tuple, "dot_indexes": [maps_all[0]], "nextdo": [maps_all[0]]}
+        maps_all.pop(0)
+        while gp["nextdo"]:
+            for n in list(gp["nextdo"].copy()):
+                neighbors, maps_all = get_neighbor(n, maps_all)
+                gp["nextdo"].remove(n)
+                gp["dot_indexes"] += neighbors
+                gp["nextdo"] += neighbors
+        gp["geocenter"] = calculate_geocenter(gp["dot_indexes"])
+        groups.append(gp)
+    return groups
 
 
 if __name__ == "__main__":
-    out = get_outline(cv2.imread(r"stylized.jpg"))
-    group(out)
-    cv2.imshow("", out)
-    cv2.waitKey(0)
+    out = cv2.imread(r"stylized.jpg")
+    out = cv2.resize(out, (384, 384))
+    out = get_outline(out)
+    o = group(out)
+    print(o)
+    img = np.zeros((384, 384, 1))
+    for a in o:
+        for i in a["dot_indexes"]:
+            img[i[0], i[1]] = 1
+        cv2.imshow("", img)
+        cv2.waitKey(0)
+    cv2.imwrite("outln.png", out)

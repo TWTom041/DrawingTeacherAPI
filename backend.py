fe5b95e5028d3799c942bec9b90b123f6e0f1146
TWTom <tom0986820022@gmail.com>
2022-11-04 11:37:55 +0800

    1

--- backend.py
+++ backend.py
@@ -1,5 +1,5 @@
 import numpy as np
-import cv2
+from cv2 import cv2
 
 import content_trans
 import outline_get
--- flask_api.py
+++ flask_api.py
@@ -1,8 +1,10 @@
-from flask import Flask, request, jsonify
-import cv2, base64
+import base64
+from cv2 import cv2
 import numpy as np
-import backend
+from flask import Flask, request, jsonify
 from flask_cors import CORS
+
+import backend
 
 app = Flask(__name__)
 CORS(app)

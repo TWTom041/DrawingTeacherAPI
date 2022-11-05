d47cfca1cf07886938fed830adc393e675de87de
TWTom <tom0986820022@gmail.com>
2022-11-05 14:21:46 +0800

    1

--- outline_get.py
+++ outline_get.py
@@ -1,5 +1,6 @@
 from cv2 import cv2
 import numpy as np
+import time
 
 
 def get_outline(image: np.ndarray, method="canny_blurred"):
@@ -24,24 +25,19 @@ def group(image, sort_method="upper"):
         oute = []
         for i in checker:
             target = (i[0] + coor[0], i[1] + coor[1])
-            if target in maps:
+            if target[0] < 0 or target[1] < 0 or target[0] >= maps.shape[0] or target[1] >= maps.shape[1]:
+                continue
+            if maps[target] != 0:
                 oute.append(target)
-                maps.remove(target)
+                maps[target] = 0
         return oute, maps
 
-    def get_map(npimg):
-        outee = []
-        for i, p in np.ndenumerate(npimg):
-            if p != 0:
-                outee.append(i)
-        return outee
-
     groups = []
-    maps_all = get_map(image)
-    while maps_all:
-        nextdo = [maps_all[0]]
-        gp = {"geocenter": tuple, "dot_indexes": [maps_all[0]]}
-        maps_all.pop(0)
+    maps_all = image
+    while (maps_all != 0).any():
+        nextdo = [(np.where(maps_all != 0)[0][0], np.where(maps_all != 0)[1][0])]
+        gp = {"geocenter": tuple, "dot_indexes": [nextdo[0]]}
+        maps_all[nextdo[0]] = 0
         while nextdo:
             for n in list(nextdo.copy()):
                 neighbors, maps_all = get_neighbor(n, maps_all)
@@ -78,9 +74,19 @@ def group(image, sort_method="upper"):
         return sorted_group
 
 
+def show_image_sorted(o):
+    for i in o:
+        for index in i["dot_indexes"]:
+            img[index] = 1
+        cv2.imshow("", img)
+        cv2.waitKey(0)
+
+
 if __name__ == "__main__":
     out = cv2.imread(r"stylized.jpg")
     out = cv2.resize(out, (384, 384))
     out = get_outline(out)
-    o = group(out, sort_method="ffg")
+    img = np.zeros((384, 384))
+    o = group(out, sort_method="nn")
+    show_image_sorted(o)
     cv2.imwrite("outln.png", out)
